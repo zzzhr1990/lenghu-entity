8503b41b3870a3f3d2ef1981f708a7c8100c4389
zzzhr <zzzhr@hotmail.com>
2021-09-28 23:56:50 +0800

    add new info

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zzzhr1990/lenghu-entity
+
+go 1.17
---
+++ offline/offline_entity.go
@@ -0,0 +1,18 @@
+package offline
+
+import "time"
+
+type OfflineInfo struct {
+	//logger *zap.Logger
+
+	ID        string    `json:"id" gorm:"primarykey, type:varchar(80)"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	URL       string    `json:"url"`
+	Torrent   string    `json:"torrent"` // Torrent File Info (Base64)
+	FileSize  int64     `json:"fileSize"`
+	Status    int       `json:"status"`
+	Type      int       `json:"type"`
+	Source    string    `json:"source"`
+	Name      string    `json:"name"`
+}
