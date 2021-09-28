afa7e451fd8b9dc1ec104caaac7391713926999d
zzzhr <zzzhr@hotmail.com>
2021-09-29 00:24:14 +0800

    spec errors

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zzzhr1990/lenghu-entity
 
 go 1.17
+
+require github.com/zzzhr1990/go-http-utilities v0.0.0-20210928154155-270ec8073141
---
+++ offline/errors.go
@@ -0,0 +1,19 @@
+package offline
+
+import (
+	"net/http"
+
+	"github.com/zzzhr1990/go-http-utilities/responses"
+)
+
+var ErrorMissingURLOrTorrentInfo = &responses.ResponseEntity{
+	Status:    http.StatusBadRequest,
+	Reference: "MISSING_TORRENT_OR_URL",
+	Message:   "You must specfic url or torrent base64 info for download",
+}
+
+var ErrorBadURLOrTorrentFile = &responses.ResponseEntity{
+	Status:    http.StatusBadRequest,
+	Reference: "BAD_URL_OR_TORRENT",
+	Message:   "Url or torrent cannot be parsed",
+}
