702af88bdffad0009db7a7de2aa246ad4714df5f
zzzhr <zzzhr@hotmail.com>
2021-10-28 16:30:10 +0800

    add ac

--- offline/offline_entity.go
+++ offline/offline_entity.go
@@ -17,6 +17,31 @@ type OfflineInfo struct {
 	Name      string    `json:"name"`
 }
 
+type OfflineQueue struct {
+	//logger *zap.Logger
+	ID        int64     `gorm:"primary_key"`
+	Hash      string    `json:"hash" gorm:"type:varchar(80)"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	URL       string    `json:"url"`
+	Torrent   string    `json:"torrent"` // Torrent File Info (Base64)
+	FileSize  int64     `json:"fileSize"`
+	Status    int       `json:"status"`
+	Type      int       `json:"type"`
+	Source    string    `json:"source"`
+	Name      string    `json:"name"`
+}
+
+type FastDownloadQueue struct {
+	//logger *zap.Logger
+	ID        int64     `gorm:"primary_key"`
+	WcsHash   string    `json:"wcsHash" gorm:"type:varchar(80)"`
+	CloudHash string    `json:"cloudHash" gorm:"type:varchar(80)"`
+	CreatedAt time.Time `json:"createdAt"`
+	FileSize  int64     `json:"fileSize"`
+	Status    int       `json:"status"`
+}
+
 type OofFileMapping struct {
 	//logger *zap.Logger
 
